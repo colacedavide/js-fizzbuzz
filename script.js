8e21ec1ba72b8f7e6a63e2d3086f28e48cfdbd0d
colacedavide <colace.davide00@gmail.com>
2025-12-03 09:56:16 +0100

    semplificazione

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-let numero= '';
+
 
 for(let i = 1; i <= 100; i++ ){
+    
+    let numero= '';
+   
     if (i % 3 === 0 && i % 5 === 0){
         numero = 'FizzBuzz';
-        console.log(numero);
+        
     }else if ( i % 3 === 0) {
         numero = 'Fizz';
-        console.log(numero);
+        
 
     }else if ( i % 5 === 0 ){
         numero = 'Buzz';
-        console.log(numero);
+        
     }else{
-       console.log(i); 
+       numero = i; 
     }
-
+      
+    console.log(numero);
     
 
 }
